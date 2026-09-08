3c51e705fbbd3edf7b6f52ff3a3791bc3e434bb0
RBDRB <raficrosby5@gmail.com>
2026-09-08 15:31:04 +0200

    Ajout de Create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 import markdown
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
@@ -35,6 +34,10 @@ class ArticleInfo(BaseModel):
     name: str
     articleUrl: str
 
+class ArticleCreate(BaseModel):
+    name: str
+    content: str
+
 
 @app.get("/")
 def root() -> dict[str, str]:
@@ -68,10 +71,14 @@ def lire_article(article_url: str) -> Article:
     name = article_url.replace(" ", "_")
     filename = name + ".md"
     article_path = ARTICLES_DIR / filename
-    name = article_url.replace("_", " ")
+    if ".." in article_url:
+        raise HTTPException(404,"The article does not exist")
+    if not article_path.exists():
+        raise HTTPException(404,"The article does not exist.")
 
     source = article_path.read_text(encoding="utf-8")
     content = markdown.markdown(source)
+    name = article_url.replace("_", " ")
 
     article = Article(
         name=name,
@@ -81,3 +88,22 @@ def lire_article(article_url: str) -> Article:
     )
 
     return article 
+
+@app.post("/create")
+def create_article(new_article: ArticleCreate):
+    name = new_article.name
+    content = new_article.content
+    source = new_article.content
+    new_article_file = ARTICLES_DIR /(name+".md")
+    new_article_file.write_text(content,encoding="utf-8")
+    article_url = name.replace(" ", "_")
+
+    content = markdown.markdown(source)
+
+    article = Article(
+        name = name,
+        articleUrl= article_url,  
+        content=content,
+        source=source
+    )
+    return article
